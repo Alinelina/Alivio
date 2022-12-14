2f55baaa93cf0b93a3aab4c87da32fca3bbffa92
alinelina <alinelina2013@gmail.com>
2022-12-14 22:48:02 +0200

    last

---
+++ main.js
@@ -0,0 +1,11 @@
+
+// let showMessageFromUser = (name, message = 'пустой текст') => {
+//     alert(`${name}: ${message}`);
+// };
+// showMessageFromUser('Алина');
+
+// let showMessage = () => {
+//     let name = prompt('Как тебя зовут?', "Алина");
+//     console.log(`Привет, ${name}`);
+// }
+// showMessage();
